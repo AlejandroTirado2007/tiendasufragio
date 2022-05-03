147e51f30377c97f54366c3a550721a834365d4e
alejandrotirado2007 <a>
2022-05-03 17:22:25 -0500

    Se aña

--- controlador.js
+++ controlador.js
@@ -1,105 +1,140 @@
 //OBJETOS
 let producto1={
-    nombre:"producto1",
-    precio:500000,
+    nombre:"GTA 5",
+    precio:130000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto2={
-    nombre:"producto2",
-    precio:500000,
+    nombre:"STREET FIGHTER 30TH ANNIVERSARY COLLECTION",
+    precio:150000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto3={
-    nombre:"producto3",
-    precio:500000,
+    nombre:"RED DEAD REDEMPTION II",
+    precio:200000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto4={
-    nombre:"producto4",
-    precio:500000,
+    nombre:"MORTAL KOMBAT 11 AFTERMATH",
+    precio:160000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto5={
-    nombre:"producto5",
-    precio:500000,
+    nombre:"RED DEAD REDEMPTION II",
+    precio:200000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto6={
-    nombre:"producto6",
-    precio:500000,
+    nombre:"PLANTS VS ZOMBIES BATTLE FOR NEIGHBORVILLE",
+    precio:140000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto7={
-    nombre:"producto7",
-    precio:500000,
+    nombre:"DRAGON BALL XENOVERSE 2",
+    precio:100000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto8={
-    nombre:"producto8",
-    precio:500000,
+    nombre:"MORTAL KOMBAT 11",
+    precio:110000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto9={
-    nombre:"producto9",
-    precio:500000,
+    nombre:"STAR WARS JEDI FALLEN ORDER",
+    precio:179000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto10={
-    nombre:"producto10",
-    precio:500000,
+    nombre:"CAPTAIN TSUBASA RISE OF NEW CHAMPIONS",
+    precio:230000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto11={
-    nombre:"producto11",
-    precio:500000,
+    nombre:"LEGO MARVEL SUPERHEROES 2",
+    precio:150000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto12={
-    nombre:"producto12",
-    precio:500000,
+    nombre:"FIFA 2022",
+    precio:300000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto13={
-    nombre:"producto13",
-    precio:500000,
+    nombre:"BATTLEFIELD V",
+    precio:210000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto14={
-    nombre:"producto14",
-    precio:500000,
+    nombre:"GRAND THEFT AUTO V PREMIUM EDITION",
+    precio:140000,
     estado:"Nuevo",
     foto:""
 }
 
 let producto15={
-    nombre:"producto15",
-    precio:500000,
+    nombre:"CALL OF DUTY BLACK OPS IV",
+    precio:160000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto16={
+    nombre:"LEGO MARVEL SUPER HEROES",
+    precio:100000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto17={
+    nombre:"JUMP FORCE",
+    precio:70000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto18={
+    nombre:"GOD OF WAR PLAYSTATION HITS",
+    precio:180000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto19={
+    nombre:"DRAGON BALL FIGHTERZ",
+    precio:120000,
+    estado:"Nuevo",
+    foto:""
+}
+
+let producto20={
+    nombre:"STAR WARS JEDI FALLEN ORDER",
+    precio:150000,
     estado:"Nuevo",
     foto:""
 }
@@ -120,7 +155,12 @@ let productos=Array(
     producto12,
     producto13,
     producto14,
-    producto15
+    producto15,
+    producto16,
+    producto17,
+    producto18,
+    producto19,
+    producto20
 )
 
 //RECORRO EL ARREGLO
@@ -128,3 +168,5 @@ productos.forEach(function(producto){
     console.log(producto.nombre)
 })
 
+
+//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
