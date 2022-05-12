94f94b43dac2e5ba47a47d8c20884d355ab65b7a
alejandrotirado2007 <aa>
2022-05-12 17:22:34 -0500

    aña

--- controlador.js
+++ controlador.js
@@ -3,140 +3,180 @@ let producto1={
     nombre:"GTA 5",
     precio:130000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto1.jpg?alt=media&token=6f290fdc-e01c-42ac-9159-7ef399c9163d"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto1.jpg?alt=media&token=6f290fdc-e01c-42ac-9159-7ef399c9163d",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto2={
     nombre:"STREET FIGHTER 30TH ANNIVERSARY COLLECTION",
     precio:150000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto10.jpg?alt=media&token=da65624d-a1e4-46f5-ad8b-dd09aeda8293"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto10.jpg?alt=media&token=da65624d-a1e4-46f5-ad8b-dd09aeda8293",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto3={
     nombre:"RED DEAD REDEMPTION II",
     precio:200000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto11.jpg?alt=media&token=722b9a2e-6299-49a6-8613-8b675744f231"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto11.jpg?alt=media&token=722b9a2e-6299-49a6-8613-8b675744f231",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto4={
     nombre:"MORTAL KOMBAT 11 AFTERMATH",
     precio:160000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto12.jpg?alt=media&token=2e5bd1cf-1843-4d05-9df9-fd3eb12b1608"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto12.jpg?alt=media&token=2e5bd1cf-1843-4d05-9df9-fd3eb12b1608",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto5={
     nombre:"RED DEAD REDEMPTION II",
     precio:200000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto13.jpg?alt=media&token=0e93762e-7c40-4371-b390-4a39a0f36e7f"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto13.jpg?alt=media&token=0e93762e-7c40-4371-b390-4a39a0f36e7f",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto6={
     nombre:"PLANTS VS ZOMBIES BATTLE FOR NEIGHBORVILLE",
     precio:140000,
-    estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto14.jpg?alt=media&token=aafce26a-cf55-44f2-a040-99fc938021ec"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto14.jpg?alt=media&token=aafce26a-cf55-44f2-a040-99fc938021ec",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto7={
     nombre:"DRAGON BALL XENOVERSE 2",
     precio:100000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto15.jpg?alt=media&token=fc6761c4-9d77-4eb2-87c9-74ad905ce857"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto15.jpg?alt=media&token=fc6761c4-9d77-4eb2-87c9-74ad905ce857",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto8={
     nombre:"MORTAL KOMBAT 11",
     precio:110000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto16.jpg?alt=media&token=bd236704-c050-4aac-b611-fa3c61ce2fde"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto16.jpg?alt=media&token=bd236704-c050-4aac-b611-fa3c61ce2fde",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto9={
     nombre:"STAR WARS JEDI FALLEN ORDER",
     precio:179000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto17.jpg?alt=media&token=6aa997a2-c0c3-4030-b08a-8002214ca86d"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto17.jpg?alt=media&token=6aa997a2-c0c3-4030-b08a-8002214ca86d",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto10={
     nombre:"CAPTAIN TSUBASA RISE OF NEW CHAMPIONS",
     precio:230000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto18.jpg?alt=media&token=7a17ce8a-9643-4a5b-abd2-775af84451aa"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto18.jpg?alt=media&token=7a17ce8a-9643-4a5b-abd2-775af84451aa",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto11={
     nombre:"LEGO MARVEL SUPERHEROES 2",
     precio:150000,
-    estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto19.jpg?alt=media&token=1700ba8a-b447-4410-ac58-7d4bdd963abc"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto19.jpg?alt=media&token=1700ba8a-b447-4410-ac58-7d4bdd963abc",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto12={
     nombre:"FIFA 2022",
     precio:300000,
     estado: true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto2.jpg?alt=media&token=1ff53a11-ee4d-43dc-b89f-1a3aae33d354"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto2.jpg?alt=media&token=1ff53a11-ee4d-43dc-b89f-1a3aae33d354",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto13={
     nombre:"BATTLEFIELD V",
     precio:210000,
-    estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto20.jpg?alt=media&token=3d4ea56c-1c04-4c5f-8fda-eefac597f351"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto20.jpg?alt=media&token=3d4ea56c-1c04-4c5f-8fda-eefac597f351",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto14={
     nombre:"GRAND THEFT AUTO V PREMIUM EDITION",
     precio:140000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto3.jpg?alt=media&token=d06a97fb-a781-4c6f-aea9-be1955111c21"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto3.jpg?alt=media&token=d06a97fb-a781-4c6f-aea9-be1955111c21",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto15={
     nombre:"CALL OF DUTY BLACK OPS IV",
     precio:160000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto4.jpg?alt=media&token=73105099-acc9-4998-b3ec-68d40318ae25"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto4.jpg?alt=media&token=73105099-acc9-4998-b3ec-68d40318ae25",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto16={
     nombre:"LEGO MARVEL SUPER HEROES",
     precio:100000,
-    estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto5.jpg?alt=media&token=7b526dd7-3f6e-40e0-8adc-b583a32b0721"
+    estado:false,
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto5.jpg?alt=media&token=7b526dd7-3f6e-40e0-8adc-b583a32b0721",
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto17={
     nombre:"JUMP FORCE",
     precio:70000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto6.jpg?alt=media&token=a0967cd2-ca5e-4f9d-bea5-57bd5ad42f3b"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto6.jpg?alt=media&token=a0967cd2-ca5e-4f9d-bea5-57bd5ad42f3b" ,
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto18={
     nombre:"GOD OF WAR PLAYSTATION HITS",
     precio:180000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto7.jpg?alt=media&token=c906fdac-ffb5-4058-8513-a17ac61a3190"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto7.jpg?alt=media&token=c906fdac-ffb5-4058-8513-a17ac61a3190" ,
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto19={
     nombre:"DRAGON BALL FIGHTERZ",
     precio:120000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto8.jpg?alt=media&token=156b30ea-9fc8-4a17-bd7b-a345bba80938"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto8.jpg?alt=media&token=156b30ea-9fc8-4a17-bd7b-a345bba80938" ,
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 let producto20={
     nombre:"STAR WARS JEDI FALLEN ORDER",
     precio:150000,
     estado:true,
-    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto9.jpg?alt=media&token=36d97656-643c-44e3-805e-21c20418cecf"
+    foto:"https://firebasestorage.googleapis.com/v0/b/tiendapabo.appspot.com/o/foto9.jpg?alt=media&token=36d97656-643c-44e3-805e-21c20418cecf" ,
+    popularidad: "★",
+    pais: "Estados unidos"
 }
 
 //ARREGLO
@@ -174,11 +214,49 @@ productos.forEach(function(producto){
     let columna = document.createElement("div")
     columna.classList.add("col")
     let tarjeta = document.createElement("div")
-    tarjeta.classList.add("card")
+    tarjeta.classList.add("card" , "h-100" , "shadow")
+
+    let nombreProducto = document.createElement("h6")
+    nombreProducto.classList.add("text-center")
+    nombreProducto.textContent = producto.nombre
+
+    let precioProducto = document.createElement("h5")
+    precioProducto.classList.add("text-center" , "text-success")
+    precioProducto.textContent = "Precio: $" + producto.precio
+
+    let estadoProducto = document.createElement("h5")
+    
+    let contenedorEstrellas = document.createElement("div")
+    contenedorEstrellas.classList.add("text-center")
+
+    let popProducto = document.createElement("h5")
+    popProducto.classList.add("text-center" , "bg-secondary")
+    popProducto.textContent = producto.popularidad
+
+    if (producto.estado == true){
+        estadoProducto.classList.add("text-center" , "text-primary")
+        estadoProducto.textContent = "Estado: Nuevo"
+    } 
+    else {
+        estadoProducto.classList.add("text-center" , "text-warning")
+        estadoProducto.textContent = "Estado: Usado"
+    }
+
+    
+
+    
 
     tarjeta.appendChild(foto)
+    tarjeta.appendChild(nombreProducto)
+    tarjeta.appendChild(precioProducto)
+    tarjeta.appendChild(estadoProducto)
+    tarjeta.appendChild(contenedorEstrellas)
+    contenedorEstrellas.appendChild(popProducto)
     columna.appendChild(tarjeta)
     contenedor.appendChild(columna)
+
+    
+    
 
 })
 
